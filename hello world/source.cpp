14741a0d4aa70a758a82f717dee8305abddc9857
105mm <king7532@knou.ac.kr>
2022-02-28 21:46:05 +0900

    hello world sc

--- hello world/source.cpp
+++ hello world/source.cpp
@@ -1,5 +1,11 @@
-#include <stdio.h>
-int main()
-{
-	printf("hello c programing \n");
-}
+/*
+예제 : Hello 출력하기
+파일 이름 : hello world.cpp
+수정 일자 : 2022년 2월 28일
+작성자 : 김세영
+*/
+#include <stdio.h>                   // 헤더 파일 추가
+
+int main() {                         // main 함수 시작
+	printf("hello c programing \n"); // 모니터에 문자열 출력
+}                                    // main 함수 끝
